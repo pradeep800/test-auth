9fe25b21fe7d5a45440088c0c6c77c07c2e733af
pradeep <pradeep8b0@gmail.com>
2023-12-04 10:17:13 +0530

    update readme

--- stacks/Secrets.ts
+++ stacks/Secrets.ts
@@ -1,9 +1,17 @@
 import { Config, StackContext } from "sst/constructs";
 export function Secret(ctx: StackContext) {
   return {
-    DATABASE_URL: new Config.Secret(ctx.stack, "DATABASE_URL"),
-    REDIS_URL: new Config.Secret(ctx.stack, "REDIS_URL"),
-    REDIS_TOKEN: new Config.Secret(ctx.stack, "REDIS_TOKEN"),
-    JWT_SECRET: new Config.Secret(ctx.stack, "JWT_SECRET"),
+    DATABASE_URL: new Config.Parameter(ctx.stack, "DATABASE_URL", {
+      value: process.env.DATABASE_URL as string,
+    }),
+    REDIS_URL: new Config.Parameter(ctx.stack, "REDIS_URL", {
+      value: process.env.REDIS_URL as string,
+    }),
+    REDIS_TOKEN: new Config.Parameter(ctx.stack, "REDIS_TOKEN", {
+      value: process.env.REDIS_TOKEN as string,
+    }),
+    JWT_SECRET: new Config.Parameter(ctx.stack, "JWT_SECRET", {
+      value: process.env.JWT_SECRET as string,
+    }),
   };
 }
